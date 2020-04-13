0fde104eac8092c66f0853660b3d61d99e8aa0ab
Alfan Ghinan Rusydi <alfangr@icloud.com>
2020-04-13 08:36:30 +0700

    update 3 page

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -9,4 +9,28 @@ class LandingController extends Controller
     public function index() {
         return view('landing.home');
     }
+
+    public function about() {
+        return view('landing.about');
+    }
+
+    public function admission_info() {
+        return view('landing.admission-info');
+    }
+
+    public function academic_info() {
+        return view('landing.academic-info');
+    }
+
+    public function academic_program() {
+        return view('landing.academic-program');
+    }
+
+    public function academic_guide() {
+        return view('landing.academic-guide');
+    }
+
+    public function study_guide() {
+        return view('landing.study-guide');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@
  * Landing Page for Portal
  */
 Route::get('/', 'LandingController@index')->name('landing.index');
+Route::get('/about-usa', 'LandingController@about')->name('landing.about');
+Route::get('/admission-info', 'LandingController@admission_info')->name('landing.admission_info');
+Route::get('/academic-info', 'LandingController@academic_info')->name('landing.academic_info');
+Route::get('/academic-program', 'LandingController@academic_program')->name('landing.academic_program');
+Route::get('/academic-guide', 'LandingController@academic_guide')->name('landing.academic_guide');
+Route::get('/study-guide', 'LandingController@study_guide')->name('landing.study_guide');
 
 /**
  * Academic
