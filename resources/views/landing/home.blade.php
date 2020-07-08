e875b4ce6e8e58cbdd4f5ff39a85233ffbcc64ea
unknown <andezapriansyah@gmail.com>
2020-07-08 09:15:20 +0700

    update berita

--- resources/views/landing/home.blade.php
+++ resources/views/landing/home.blade.php
@@ -892,6 +892,28 @@
                             <div class="item">
                                 <div class="recent-news">
                                     <div class="action-box">
+                                        <img src="{{ asset('homepages/images/berita/beranda/lima.jpg') }}" alt="">
+                                    </div>
+                                    <div class="info-bx">
+                                        <ul class="media-post">
+                                            <li><a href="#"><i class="fa fa-calendar"></i>Juli 7, 2019</a></li>
+                                            <li><a href="#"><i class="fa fa-user"></i>By detik.com</a></li>
+                                        </ul>
+                                        <h5 class="post-title"><a
+                                                href="https://news.detik.com/berita/d-5084211/pemerintah-siapkan-rp-22-t-untuk-kembangkan-pendidikan-berbasis-digital?_ga=2.62179176.514164471.1594171764-1740682019.1594171764">Pemerintah Siapkan Rp 22 T untuk Kembangkan Pendidikan Berbasis Digital</a></h5>
+                                        <p>Jakarta - Pemerintah akan mengembangkan pendidikan berbasis digital di seluruh wilayah. Pemerintah menyiapkan dana sekitar ...</p>
+                                        <div class="post-extra">
+                                            <a href="https://news.detik.com/berita/d-5084211/pemerintah-siapkan-rp-22-t-untuk-kembangkan-pendidikan-berbasis-digital?_ga=2.62179176.514164471.1594171764-1740682019.1594171764"
+                                                class="btn-link">READ MORE</a>
+                                            {{-- <a href="#" class="comments-bx"><i class="fa fa-comments-o"></i>20 Comment</a> --}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <div class="recent-news">
+                                    <div class="action-box">
                                         <img src="{{ asset('homepages/images/berita/beranda/satu.jpg') }}" alt="">
                                     </div>
                                     <div class="info-bx">
